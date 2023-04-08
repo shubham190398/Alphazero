9498dacb5b83e0f541421a4c5ed91d6abfc6d076
Shubham Acharya <shubham190398@gmail.com>
2023-04-08 13:27:05 +0530

    Cleanup and Testing on a state

--- alpha_tictactoe.py
+++ alpha_tictactoe.py
@@ -169,11 +169,15 @@ def model_visualize():
     state = tictactoe.get_initial_state()
     state = tictactoe.get_next_state(state, 2, 1)
     state = tictactoe.get_next_state(state, 6, -1)
+    state = tictactoe.get_next_state(state, 8, 1)
+    state = tictactoe.get_next_state(state, 4, -1)
 
     print("State is currently", state)
 
     tensor_state = torch.tensor(tictactoe.get_encoded_state(state)).unsqueeze(0)
     model = ResNet(tictactoe, 4, 64)
+    model.load_state_dict(torch.load('models/model_3.pt'))
+    model.eval()
 
     policy, value = model(tensor_state)
     policy = torch.softmax(policy, axis=1).squeeze(0).detach().cpu().numpy()
@@ -246,4 +250,4 @@ def alphaTrain():
     alphaZero.learn()
 
 
-alphaTrain()
+model_visualize()
