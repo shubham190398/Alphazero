765191079ddc1f2156646d364f6bf86da95685c2
Shubham Acharya <shubham190398@gmail.com>
2023-04-08 12:32:27 +0530

    Added learn method for AlphaZero Class

--- alphazero.py
+++ alphazero.py
@@ -1,6 +1,7 @@
 """
 Contains the Alphazero class for training a model to play TicTacToe or ConnectFour
 """
+import torch
 
 # Importing dependencies
 from alphamontecarlo import AlphaMCTS
@@ -17,3 +18,28 @@ class AlphaZero:
         self.game = game
         self.args = args
         self.mcts = AlphaMCTS(game, args, model)
+
+    def selfPlay(self):
+        pass
+
+    def train(self, memory):
+        pass
+
+    """
+    For each iteration create training data for one cycle, train the model and
+    store the state of the model
+    """
+    def learn(self):
+        for iteration in range(self.args['num_iterations']):
+            memory = []
+            self.model.eval()
+
+            for selfPlay_iteration in range(self.args['num_selfPlay_iterations']):
+                memory += self.selfPlay()
+
+            self.model.train()
+            for epoch in range(self.args['num_epochs']):
+                self.train(memory)
+
+            torch.save(self.model.state_dict(), f"model_{iteration}.pt")
+            torch.save(self.optimizer.state_dict(), f"optimizer_{iteration}.pt")
